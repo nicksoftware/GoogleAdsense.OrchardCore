0a08b28986c5e6a1fbb8eedbf8da2fdbea659a2d
NICOLAS MALULEKE <nicolusmaluleke@gmail.com>
2020-10-29 16:05:25 +0200

    Initial Commit

---
+++ src/GoogleAdsense.OrchardCore/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "GoogleAdsense.OrchardCore",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "GoogleAdsense.OrchardCore",
+    Category = "Content Management"
+)]
---
+++ src/GoogleAdsense.OrchardCore/Startup.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace GoogleAdsense.OrchardCore
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            routes.MapAreaControllerRoute(
+                name: "Home",
+                areaName: "GoogleAdsense.OrchardCore",
+                pattern: "Home/Index",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+        }
+    }
+}
