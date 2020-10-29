8b88b87edb4114f3a32799b4adba756e1cabf310
NICOLAS MALULEKE <nicolusmaluleke@gmail.com>
2020-10-30 01:27:25 +0200

    Added module description

--- src/GoogleAdsense.OrchardCore/obj/Debug/netcoreapp3.1/GoogleAdsense.OrchardCore.AssemblyInfo.cs
+++ src/GoogleAdsense.OrchardCore/obj/Debug/netcoreapp3.1/GoogleAdsense.OrchardCore.AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nicolas Maluleke")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0-rc2-13450")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoogleAdsense.OrchardCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoogleAdsense.OrchardCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
